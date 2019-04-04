54a925e86a91ec68a8f66be7e9a8d07015493a14
Yaniv Aknin <yaniv@aknin.name>
2019-04-04 22:11:53 +0000

    compact lines in reactor

--- reactor.py
+++ reactor.py
@@ -3,17 +3,6 @@ import collections
 import time
 
 class Reactor(object):
-    def __enter__(self):
-        self.handles = collections.OrderedDict()
-        for name, controller in self.controllers.items():
-            self.handles[name] = controller.__enter__()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        for name, handle in self.handles.items():
-            handle.__exit__(exc_type, exc_val, exc_tb)
-        self.handles = None
-
     def __init__(self, delay=0.1):
         self.delay = 0.1
         self.controllers = collections.OrderedDict()
@@ -21,10 +10,17 @@ class Reactor(object):
         self.loggers = []
         self.timers = []
         self.handles = None
-
+    def __enter__(self):
+        self.handles = collections.OrderedDict()
+        for name, controller in self.controllers.items():
+            self.handles[name] = controller.__enter__()
+        return self
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for name, handle in self.handles.items():
+            handle.__exit__(exc_type, exc_val, exc_tb)
+        self.handles = None
     def __getattr__(self, name):
         return self.handles[name]
-
     def loop(self):
         self.variables['delay'] = 0
         assert self.handles is not None
@@ -43,18 +39,14 @@ class Reactor(object):
             self.variables['delay'] = current - previous
             self.variables['timestamp'] = current
             previous = current
-
     def log(self, text):
         for logger in self.loggers:
             logger(text)
-
     def stop(self):
         self.running = False
-
     def schedule_once(self, duration, func):
         when = time.time() + duration
         heapq.heappush(self.timers, (when, 0, func))
-
     def schedule_recurring(self, interval, func):
         when = time.time() + interval
         heapq.heappush(self.timers, (when, interval, func))
